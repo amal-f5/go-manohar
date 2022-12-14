2bd982c747b01d80d6bdeb8c94080141b802abc4
Amal Rajan <107884199+amal-f5@users.noreply.github.com>
2022-12-14 14:26:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,16 +9,17 @@ import (
 
 func main() {
 	log.Printf("Starting server at port 8080")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Home")
-	})
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Eve!")
-	})
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		return
-	})
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Home")
+// 	})
+// 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Eve!")
+// 	})
+// 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+// 		return
+// 	})
 	if err := unit.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
+		io.WriteString(w, "Hello, Go demo app!")
 	}
 }
