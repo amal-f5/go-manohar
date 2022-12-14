ba730f0ce845fe2f540ce46ccf164ed50b8f7f05
Amal Rajan <107884199+amal-f5@users.noreply.github.com>
2022-12-14 14:31:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"unit.nginx.org/go"
 )
+// To get this up working with Eve, add unit.nginx.org/go in the imports
 
 func main() {
 	log.Printf("Starting server at port 8080")
@@ -18,6 +19,8 @@ func main() {
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		return
 	})
+	
+	// Here, change http.ListenAndServer -> unit.ListenAndServe
 	if err := unit.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
 	}
